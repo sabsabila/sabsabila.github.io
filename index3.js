ad3640c66419dd2b16fc3a13795641b6a1d79a62
Sabila Jamal <sabilajml@gmail.com>
2023-03-13 15:08:03 +0700

    fix import

--- index3.js
+++ index3.js
@@ -1,7 +1,7 @@
-import { GLTFLoader } from '/three/addons/loaders/GLTFLoader.js';
-import { RoomEnvironment } from '/three/addons/environments/RoomEnvironment.js';
-import { FontLoader } from '/three/addons/loaders/FontLoader.js';
-import { TextGeometry } from '/three/addons/geometries/TextGeometry.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 //import * as gsap from "gsap";
 
 //#region variables
